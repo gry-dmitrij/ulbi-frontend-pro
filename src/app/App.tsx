50f536c8b0c9b215922c93975228626ec99039ae
gryda <gryda@hismith.ru>
2024-04-10 21:08:27 +0300

    lesson 2.1 Конфиг роутера

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,9 +2,10 @@ import { Link, Route, Routes } from 'react-router-dom'
 import './styles/index.scss'
 import { Suspense } from 'react'
 import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme'
-import { classNames } from 'helpers/classNames/classNames'
+import { classNames } from 'shared/lib/classNames/classNames'
 import { AboutPage } from 'pages/AboutPage'
 import { MainPage } from 'pages/MainPage'
+import { AppRouter } from 'app/providers/router'
 
 const App = () => {
   const {theme, toggleTheme} = useTheme()
@@ -13,12 +14,7 @@ const App = () => {
     <button onClick={toggleTheme}>Toggle theme</button>
     <Link to={'/'}>Главная</Link>
     <Link to={'/about'}>О сайте</Link>
-    <Suspense fallback={<div>Loading</div>}>
-      <Routes>
-        <Route path={'/about'} element={<AboutPage />} />
-        <Route path={'/'} element={<MainPage />} />
-      </Routes>
-    </Suspense>
+    <AppRouter />
   </div>
 }
 
