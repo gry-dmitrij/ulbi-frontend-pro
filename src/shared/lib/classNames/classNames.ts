90dcef862aab7ba3300dad81e72bf85f03e36872
gryda <gryda@hismith.ru>
2024-04-17 11:34:34 +0300

    add jest

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -4,8 +4,8 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
   return [
     cls,
     ...additional.filter(Boolean),
-    Object.entries(mods)
-      .filter(([_, value]) => !!value)
+    ...Object.entries(mods)
+      .filter(([_, value]) => Boolean(value))
       .map(([cls]) => cls),
   ].join(' ');
 }
